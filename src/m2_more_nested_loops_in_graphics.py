6212d4db28f1eb16bade66fab749911475d3858b
conradja <conradja@rose-hulman.edu>
2019-01-21 10:42:38 -0500

    finished

--- src/m2_more_nested_loops_in_graphics.py
+++ src/m2_more_nested_loops_in_graphics.py
@@ -4,8 +4,8 @@ in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Joseph Conrad.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -50,10 +50,27 @@ def draw_upside_down_wall(rectangle, n, window):
     and n is nonnegative.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     Some tests are already written for you (above).
     # -------------------------------------------------------------------------
+    Lx = rectangle.get_upper_left_corner().x
+    Ly = rectangle.get_upper_left_corner().y
+    delta_x = rectangle.get_width() /2
+    height = rectangle.get_height()
+    Rx = rectangle.get_lower_right_corner().x
+    Ry = rectangle.get_lower_right_corner().y
 
+    for j in range(n):
+        for x in range(j+1):
+            rect = rg.Rectangle(rg.Point(Lx,Ly), rg.Point(Rx,Ry))
+            rect.attach_to(window)
+            window.render(0.1)
+            Lx+= rectangle.get_width()
+            Rx+= rectangle.get_width()
+        Lx = rectangle.get_upper_left_corner().x - delta_x*(j+1)
+        Rx = rectangle.get_lower_right_corner().x - delta_x*(j+1)
+        Ly -= height
+        Ry -= height
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
